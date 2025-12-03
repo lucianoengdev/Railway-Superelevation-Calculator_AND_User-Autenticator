f0034604c9106ad08c28f6089f27ab63a3bd93a9
Luciano Faria <luciano15faria@gmail.com>
2025-12-02 21:22:09 -0300

    1.0.0 Configured a new model of User

--- Program/models.py
+++ Program/models.py
@@ -1,4 +1,9 @@
-from .init import con
-import sqlite3
+from .init import app, db
 
-class user():
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    email = db.Column(db.String, nullable=False, unique=True)
+    password = db.Column(db.String, nullable=False)
+        
