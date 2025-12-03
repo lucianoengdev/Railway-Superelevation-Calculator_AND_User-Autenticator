86c4a0e92bbcc0c0d22f3b189df3424565a5f805
Luciano Faria <luciano15faria@gmail.com>
2025-12-02 21:20:04 -0300

    1.0.0 Update class to insert new info in db

--- Program/forms.py
+++ Program/forms.py
@@ -5,7 +5,7 @@ from wtforms.validators import Length, DataRequired
 
 class registro(FlaskForm):
     name = StringField('Nome', validators=[DataRequired()])
-    user = StringField('Nome de Usuário', validators=[Length(max=15), DataRequired()])
+    email = StringField('Email', validators=[Length(max=30), DataRequired()])
     password1 = StringField('Senha', validators=[Length(min=6, max=12), DataRequired()])
     password2 = StringField('Confirme sua senha', validators=[Length(min=6, max=12), DataRequired()])
 
